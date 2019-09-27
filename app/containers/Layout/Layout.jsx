bc799d88d0b081811e40400b799c69f1c49b2c35
alexzerntev <alexzerntev@gmail.com>
2019-09-27 21:00:25 +0300

    added error boundary again

--- app/containers/Layout/Layout.jsx
+++ app/containers/Layout/Layout.jsx
@@ -6,27 +6,34 @@ import Employees from "./Employees/Employees";
 import Footer from "./Footer/Footer";
 
 import API from "../../utils/API";
+import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
 const Layout = () => {
 
   const [user, setUser] = useState(null);
+  const [hasError, setHasError] = useState(null);
 
   useEffect(() => {
     (async () => {
       try {
-        const user = API.get("user");
+        const user = await API.get("user");
         setUser(user.data);
+        setHasError(false);
       } catch (error) {
-
+        setHasError(true);
       }
     })();
   }, []);
 
   return (
     <Container>
-      <Header user={user} />
-      <Employees />
-      <Footer user={user} />
+      <ErrorBoundary hasError={hasError}>
+        <Header user={user} />
+        <ErrorBoundary>
+          <Employees />
+        </ErrorBoundary>
+        <Footer user={user} />
+      </ErrorBoundary>
     </Container>
   );
 };
